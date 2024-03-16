e3fb233d4ee397cc73e15efda588c783a0cc6de7
unknown <laveshvyas20@gmail.com>
2024-03-16 15:18:32 +0530

    init

--- controllers/url.js
+++ controllers/url.js
@@ -1,9 +1,10 @@
 const {shortid} = require('shortid');
 const URL = require('../models/url');
+
 async function  handleGenerateShortUrl (req,res){
     const body = req.body;
     if(!body.url) res.status(500).json({message:"URL required"})
-     const shortURL = shortid()
+     const shortURL = shortid
     await URL.create({
         shortid:shortURL,
         redirectURL:body.url,
--- models/url.js
+++ models/url.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const urlSchema = new mongoose.Schema({
-        shortId:{
+        shortid:{
             type:String,
             required:true,
             unique:true
