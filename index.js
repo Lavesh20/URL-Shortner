ea42b95f7dfafd0e35a157b16d7895641b0c12d2
unknown <laveshvyas20@gmail.com>
2024-03-16 19:24:31 +0530

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const urlRoute = require("./routes/route_url")
 const {connectToMongoDB} = require('./connect');
+const URL = require('./models/url');
 
 connectToMongoDB('mongodb+srv://laveshvyas20:HYfPIVV7timUKqPN@cluster0.frfboac.mongodb.net/Short-URL')
 .then(()=> console.log("MongoDb connected"))
@@ -10,6 +11,20 @@ const app = express();
 app.use(express.json())
 
 app.use("/url",urlRoute);
+
+app.get('/:shortId',async(req,res)=>{
+ const shortId = req.param.shortId;
+ const entry = await URL.findOneAndUpdate({
+    shortId
+ },{
+    $push:{
+        visitHistory:{
+            timestamp: new Date()
+        },
+    },
+ })
+  res.redirect(entry.redirectURL)
+})
 
 app.listen(8000,()=>{
     console.log("Server started at Port 8000");
--- models/url.js
+++ models/url.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const shortid = require('shortid');
 
 const urlSchema = new mongoose.Schema({
         shortid:{
             type:String,
             required:true,
-            unique:true
-        },
+            unique:true,
+            default: shortid()
+            },
         redirectURL:{
             type:String,
             required:true
