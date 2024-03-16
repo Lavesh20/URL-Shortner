7ba353eb6f19a8ba13b96ff2998a48015a79afd2
unknown <laveshvyas20@gmail.com>
2024-03-16 12:03:33 +0530

    init

---
+++ controllers/url.js
@@ -0,0 +1,15 @@
+const {shortid} = require('shortid');
+const URL = require('../models/url');
+async function  handleGenerateShortUrl (req,res){
+    const body = req.body;
+    if(!body.url) res.status(500).json({message:"URL required"})
+     const shortURL = shortid()
+    await URL.create({
+        shortid:shortURL,
+        redirectURL:body.url,
+        visitHistory:[]
+    })
+}
+module.exports = {
+    handleGenerateShortUrl
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const urlRoute = require("./routes/route_url")
+const {connectToMongoDB} = require('./connect');
+
+connectToMongoDB('mongodb+srv://laveshvyas20:HYfPIVV7timUKqPN@cluster0.frfboac.mongodb.net/Short-URL')
+.then(()=> console.log("MongoDb connected"))
+const app = express();
+app.use("/url",urlRoute);
+app.listen(8000,()=>{
+    console.log("Server started at Port 8000");
+})
